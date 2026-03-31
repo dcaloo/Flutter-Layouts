2b8d7abd2ae248b7345114bcf6ddbc7292801e4f
dcaloo <eduardo.calo0304@gmail.com>
2026-03-31 08:53:43 -0300

    Add Column

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,48 @@
+// CONTAINER
+
+// import 'package:flutter/material.dart';
+ 
+// void main() {
+//   runApp(MyApp());
+// }
+ 
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 200,
+//             height: 200,
+//             padding: const EdgeInsets.all(20),
+//             decoration: BoxDecoration(
+//               color: Colors.blue,
+//               borderRadius: BorderRadius.circular(15),
+//               boxShadow: const [
+//                 BoxShadow(
+//                   color: Colors.black26,
+//                   blurRadius: 10,
+//                   spreadRadius: 2,
+//                 ),
+//               ],
+//             ),
+//             child: const Center(
+//               child: Text(
+//                 'Olá, Flutter!',
+//                 style: TextStyle(color: Colors.white, fontSize: 18),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// COLUMN
+
 import 'package:flutter/material.dart';
  
 void main() {
@@ -9,29 +54,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 10,
-                  spreadRadius: 2,
-                ),
-              ],
-            ),
-            child: const Center(
-              child: Text(
-                'Olá, Flutter!',
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-          ),
+        appBar: AppBar(title: const Text('Exemplo de Column')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(color: Colors.red, height: 50, width: 100),
+            Container(color: Colors.green, height: 50, width: 150),
+            Container(color: Colors.blue, height: 50, width: 200),
+          ],
         ),
       ),
     );
